0d35419f1423c521f4aad3a56119b22eb5c10cd4
JiMyoungHa <JiMyoungHa@DESKTOP-O0MNEAS>
2018-10-08 14:42:58 +0900

    캐릭터, 맵 오브젝트화 및 패키지 src 설정

--- src/online/gamgineer/game/gui/GameFrame.java
+++ src/online/gamgineer/game/gui/GameFrame.java
@@ -6,6 +6,8 @@ import java.awt.Graphics;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import online.gamgineer.game.object.GameCharacter;
+
 public class GameFrame extends JFrame {
 
 	private static final long serialVersionUID = 1L;
@@ -37,7 +39,7 @@ class GamePanel extends JPanel {
 	private int frame_width_size;
 	private int frame_height_size;
 
-	private GameObject playableCharacter;
+	private GameCharacter playableCharacter;
 
 	public GamePanel(int frame_width_size, int frame_height_size) {
 		this.frame_width_size = frame_width_size;
@@ -52,14 +54,14 @@ class GamePanel extends JPanel {
 	}
 
 	public void createObject() {
-		this.playableCharacter = new GameObject(frame_width_size / 2, frame_height_size / 2, 8, 16, Color.RED);
+		this.playableCharacter = new GameCharacter(frame_width_size / 2, frame_height_size / 2, 8, 16, Color.RED);
 	}
 
 	public void drawObject(Graphics g) {
 		this.playableCharacter.draw(g);
 	}
 
-	public GameObject getPC() {
+	public GameCharacter getPC() {
 		return this.playableCharacter;
 	}
 
@@ -72,50 +74,6 @@ class GamePanel extends JPanel {
 			}
 		}
 		this.repaint();
-	}
-
-}
-
-class GameObject {
-
-	private int obj_pos_x; // 게임 객체 X 좌표값
-	private int obj_pos_y; // 게임 객체 Y 좌표값
-	private int obj_width; // 게임 객체 가로 길이
-	private int obj_height; // 게임 객체 세로 길이
-	private Color obj_color;
-
-	public GameObject(int obj_pos_x, int obj_pos_y, int obj_width, int obj_height, Color obj_color) {
-		this.obj_pos_x = obj_pos_x;
-		this.obj_pos_y = obj_pos_y;
-		this.obj_width = obj_width;
-		this.obj_height = obj_height;
-		this.obj_color = obj_color;
-	}
-
-	public void draw(Graphics g) {
-		g.setColor(this.obj_color);
-		g.fillRect(obj_pos_x, obj_pos_y, obj_width, obj_height);
-	}
-
-	public void move(int dx, int dy) {
-		this.obj_pos_x = this.obj_pos_x + dx;
-		this.obj_pos_y = this.obj_pos_y + dy;
-	}
-
-	public int getX() {
-		return this.obj_pos_x;
-	}
-
-	public int getY() {
-		return this.obj_pos_y;
-	}
-
-	public int getW() {
-		return this.obj_width;
-	}
-
-	public int getH() {
-		return this.obj_height;
 	}
 
 }
