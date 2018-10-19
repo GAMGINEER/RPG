0e3e66bf147f5825dac1199ed8273a45d487f2ff
Ji Myoung Ha <noplayer40600@gmail.com>
2018-10-19 17:06:12 +0900

    KeyMapping 클래스 추가

---
+++ src/online/gamgineer/game/control/KeyMapping.java
@@ -0,0 +1,32 @@
+package online.gamgineer.game.control;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class KeyMapping implements KeyListener{
+	
+	private static final int SPEED = 10;
+	
+	public KeyMapping() {
+		
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
