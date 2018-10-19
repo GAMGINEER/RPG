4db72778e2be5fa8a9998541e8d8fa9b6d1a00ea
JiMyoungHa <JiMyoungHa@DESKTOP-O0MNEAS>
2018-10-19 14:42:07 +0900

    주석 추가

--- src/online/gamgineer/game/gui/GameFrame.java
+++ src/online/gamgineer/game/gui/GameFrame.java
@@ -17,18 +17,26 @@ public class GameFrame extends JFrame {
 	private static final int FRAME_HEIGHT_SIZE = 600; // (상수) GameFrame 세로 길이
 
 	public GameFrame() {
+
+		// 게임창 제목
 		this.setTitle("GameFrame");
-		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		this.setSize(FRAME_WIDTH_SIZE, FRAME_HEIGHT_SIZE);
-		this.setLocationRelativeTo(null);
-		this.setResizable(false);
-		this.setVisible(true);
 
-		GamePanel gp = new GamePanel(FRAME_WIDTH_SIZE, FRAME_HEIGHT_SIZE);
-		this.addKeyListener(new GameControl(gp));
+		// 게임창 설정
+		this.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE); // 창을 닫을 때 프로그램 종료가 아니라 창만 사라지게 함
+		this.setLocationRelativeTo(null); // 창 위치 초기화
+		this.setResizable(false); // 창 크기 조절기능 해제
+		this.setVisible(true); // 창 보이기
+		// this.setSize(FRAME_WIDTH_SIZE, FRAME_HEIGHT_SIZE);
 
-		this.add(gp);
-		this.pack();
+		// 게임 패널 설정
+		GamePanel gp = new GamePanel(FRAME_WIDTH_SIZE, FRAME_HEIGHT_SIZE); // 게임 패널 추가
+		this.addKeyListener(new GameControl(gp)); // 게임 패널에 입력키 리스너 추가
+		this.add(gp); // 게임 창에 게임 패널 추가
+
+		// Container gameFrameContentPane = this.getContentPane();
+		// gameFrameContentPane.setBackground(Color.BLACK);
+
+		this.pack(); // 게임창 크기 조정
 	}
 
 }
@@ -45,7 +53,8 @@ class GamePanel extends JPanel {
 	public GamePanel(int frame_width_size, int frame_height_size) {
 		this.frame_width_size = frame_width_size;
 		this.frame_height_size = frame_height_size;
-		this.setPreferredSize(frame_width_size, frame_height_size);
+		this.setPreferredSize(new Dimension(frame_width_size, frame_height_size));
+		this.setBackground(Color.WHITE);
 		createObject();
 	}
 
@@ -76,10 +85,6 @@ class GamePanel extends JPanel {
 			}
 		}
 		this.repaint();
-	}
-
-	private void setPreferredSize(int frameWidthSize, int frameHeightSize) {
-		super.setPreferredSize(new Dimension(frameWidthSize, frameHeightSize));
 	}
 
 }
