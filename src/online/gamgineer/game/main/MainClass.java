c1fc8f85a071be8d5099a02804559081eec4fec0
grandmasterrabbit <kkito9999@gmail.com>
2018-11-20 20:40:00 +0900

    50마리의 토끼

--- src/online/gamgineer/game/main/MainClass.java
+++ src/online/gamgineer/game/main/MainClass.java
@@ -6,7 +6,7 @@ public class MainClass {
 	
 	public static int GAME_FRAME_WIDTH = 600;
 	public static int GAME_FRAME_HEIGHT = 600;
-	public static int PLAYER_SPEED = 10;
+	public static int PLAYER_SPEED = 10*5;
 
 	public static void main(String[] args) {
 		new StartFrame();
