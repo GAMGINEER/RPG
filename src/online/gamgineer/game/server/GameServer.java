193a3b57a3833eb51cadcaa417b09cd6e10fa667
LeeSK78944 <leesk78944@gmail.com>
2018-12-04 10:16:06 +0900

    Server
    서버구현

--- src/online/gamgineer/game/server/GameServer.java
+++ src/online/gamgineer/game/server/GameServer.java
@@ -1,50 +1,55 @@
 package online.gamgineer.game.server;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class GameServer {
 	private final int PORT = 8080;
-	private ServerSocket server;
-	private Socket client;
-	
-	//서버셋팅
+	private ServerSocket server=null;
+	private Socket client=null;
+	private ExecutorService tf = Executors.newCachedThreadPool();
+
+	// 서버셋팅
 	public void serverSetting() {
 		try {
-			while(true) {
+			while (true) {
 				server = new ServerSocket(PORT);
 				client = server.accept();
 				joinClient reicever = new joinClient(client);
-				reicever.start();
+				tf.execute(reicever);
 			}
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
-	
-	//클라이언트 접속 쓰레드
-	class joinClient extends Thread {
-		private ObjectOutputStream oos;
-		private ObjectInputStream ois;
-		
+
+	// 클라이언트 접속 쓰레드
+	public class joinClient extends Thread {
+		private DataOutputStream dos=null;
+		private DataInputStream dis=null;
+		private Socket socket=null;
+
 		public joinClient(Socket socket) {
-			try {
-				oos = new ObjectOutputStream(socket.getOutputStream());
-				ois = new ObjectInputStream(socket.getInputStream());
-			}catch(IOException e) {
-				e.printStackTrace();
-			}
+			this.socket = socket;
 		}
-		
+
 		@Override
 		public void run() {
+			try {
+				dos = new DataOutputStream(socket.getOutputStream());
+				dis = new DataInputStream(socket.getInputStream());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
 			
 		}
 	}
-	
+
 	public static void main(String[] args) {
 		GameServer GS = new GameServer();
 		GS.serverSetting();
