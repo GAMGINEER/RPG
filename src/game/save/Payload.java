23bfd7ba67ae34e7876f8f2ac3cc0bd761953e9d
JiMyoungHa <JiMyoungHa@DESKTOP-AHNBFH7>
2018-12-17 00:23:32 +0900

    Payload Class 추가

---
+++ src/game/save/Payload.java
@@ -0,0 +1,46 @@
+package game.save;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Serializable;
+
+import game.io.ObjectSave;
+import game.object.Player;
+
+public class Payload implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	private String userID;
+	private Save save;
+	private Player player;
+
+	public Payload(String userID) {
+		this.userID = userID;
+		try {
+			this.save = (Save) ObjectSave.Input("save");
+			this.player = (Player) ObjectSave.Input("player");
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		} catch (NullPointerException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public String getUserID() {
+		return this.userID;
+	}
+
+	public Save getSave() {
+		return this.save;
+	}
+
+	public Player getPlayer() {
+		return this.player;
+	}
+
+}
