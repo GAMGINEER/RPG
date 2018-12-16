7b475622b30f6d8957159da34560eb568ba5a2c8
LeeSK78944 <leesk78944@gmail.com>
2018-12-16 23:30:30 +0900

    Object 입출력
    만드는중
    틀만 짬

---
+++ src/game/server/ObjectIOClient.java
@@ -0,0 +1,61 @@
+package game.server;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+
+public class ObjectIOClient {
+	private static final int SERVER_PORT = 8100;
+	private static final String SERVER_IP = "edu.gamgineer.online";
+	private Socket client;
+	private ObjectOutputStream output;
+	private ObjectInputStream input;
+	
+	public ObjectIOClient() {
+		connect();
+		setStream();
+		dataSend();
+		dataReceive();
+	}
+	
+	public void connect() {
+		try {
+			client = new Socket(SERVER_IP, SERVER_PORT);
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setStream() {
+		try {
+			output = new ObjectOutputStream(client.getOutputStream());
+			output.flush();
+			input = new ObjectInputStream(client.getInputStream());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void dataReceive() {
+		Runnable rDr = new Runnable() {
+			@Override
+			public void run() {
+				
+			}
+		};
+		Thread tDr = new Thread(rDr);
+		tDr.start();
+	}
+	
+	public void dataSend() {
+		Runnable rDs = new Runnable() {
+			@Override
+			public void run() {
+				
+			}
+		};
+		Thread tDs = new Thread(rDs);
+		tDs.start();
+	}
+}
---
+++ src/game/server/ObjectIOServer.java
@@ -0,0 +1,5 @@
+package game.server;
+
+public class ObjectIOServer {
+	private static final int SERVER_PORT = 8100;
+}
