4870eaccfb32922de80cc7506b0287da193c549d
LeeSK78944 <leesk78944@gmail.com>
2018-12-17 03:15:51 +0900

    로그인 창
    회원가입 구현 및 체크기능 구현

--- src/game/graphics/LoginFrame.java
+++ src/game/graphics/LoginFrame.java
@@ -3,29 +3,32 @@ package game.graphics;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.HashMap;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JPasswordField;
 import javax.swing.JTextField;
 
 public class LoginFrame extends JFrame implements ActionListener {
-	private String ID="abcd1234", PW="abcd1234";
+	private HashMap<String, String> ac = new HashMap<>();
 	private JTextField txtID;
 	private JPasswordField txtPW;
-	private JButton login;
+	private JButton login, account;
 	private JLabel loginText = new JLabel();
 	
 	public LoginFrame() {
-		setLayout(new GridLayout(4, 1));
+		setLayout(new GridLayout(3, 1));
 		setTitle("로그인 창");
 		setSize(300, 200);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
 		JPanel panelID = new JPanel();
 		JPanel panelPW = new JPanel();
+		JPanel panelBtn = new JPanel();
 		
 		txtID = new JTextField(12);
 		txtPW = new JPasswordField(12);
@@ -33,8 +36,10 @@ public class LoginFrame extends JFrame implements ActionListener {
 		JLabel labelID = new JLabel("ID ");
 		JLabel labelPW = new JLabel("PW ");
 		
-		login = new JButton("LOGIN");
+		login = new JButton("로그인");
 		login.addActionListener(this);
+		account = new JButton("회원가입");
+		account.addActionListener(this);
 		
 		panelID.add(labelID);
 		panelID.add(txtID);
@@ -42,9 +47,12 @@ public class LoginFrame extends JFrame implements ActionListener {
 		panelPW.add(labelPW);
 		panelPW.add(txtPW);
 		
+		panelBtn.add(login);
+		panelBtn.add(account);
+		
 		add(panelID);
 		add(panelPW);
-		add(login);
+		add(panelBtn);
 		add(loginText);
 		
 		setVisible(true);
@@ -53,10 +61,68 @@ public class LoginFrame extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		if(e.getSource()==login) {
 			String str = String.valueOf(txtPW.getPassword());
-			if(ID.equals(txtID.getText())&&PW.equals(str)) {
-				loginText.setText("로그인 성공");
-			}else {
+			int i=0;
+			for(i=0; i<ac.size(); i++) {
+				if(ac.containsKey(txtID.getText())&& ac.get(txtID.getText()).equals(str)) {
+					loginText.setText("로그인 성공");
+					break;
+				}
+			}
+			if(i==ac.size())
 				loginText.setText("로그인 실패");
+		}
+		if(e.getSource()==account)
+			new Account();
+	}
+	
+	class Account extends JFrame implements ActionListener {
+		private JTextField acID, acPW;
+		private JButton cf;
+		
+		public Account() {
+			setLayout(new GridLayout(3, 1));
+			setTitle("회원가입");
+			setSize(100, 200);
+			
+			acID = new JTextField(10);
+			acPW = new JTextField(10);
+			
+			JPanel panelID = new JPanel();
+			JPanel panelPW = new JPanel();
+			
+			JLabel labelID = new JLabel("ID ");
+			JLabel labelPW = new JLabel("PW ");
+			
+			cf = new JButton("가입하기");
+			cf.addActionListener(this);
+			
+			panelID.add(labelID);
+			panelID.add(acID);
+			
+			panelPW.add(labelPW);
+			panelPW.add(acPW);
+			
+			add(panelID);
+			add(panelPW);
+			add(cf);
+			
+			setVisible(true);
+		}
+		
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == cf) {
+				int j=0;
+				for(j=0; j<ac.size(); j++) {
+					if(ac.containsKey(acID.getText())) {
+						JOptionPane.showMessageDialog(null, "이미 있는 계정입니다", "System", JOptionPane.ERROR_MESSAGE);
+						break;
+					}
+				}
+				if(j==ac.size()) {
+					ac.put(acID.getText(), acPW.getText());
+					JOptionPane.showMessageDialog(null, "회원가입 완료");
+					dispose();
+				}
 			}
 		}
 	}
