d2b19805c4a0021b9e63267e933462a310d16b9e
LeeSK78944 <leesk78944@gmail.com>
2018-12-17 15:04:58 +0900

    채팅서버수정
    수정

--- src/game/graphics/ChatFrame.java
+++ src/game/graphics/ChatFrame.java
@@ -20,31 +20,32 @@ import javax.swing.JPanel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
+import game.server.ReceiveDataThread;
+
 public class ChatFrame extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = 1L;
-	private static final String SERVER_IP = "edu.gamgineer.online";
-	private static final int SERVER_PORT = 8100;
+	private String ip;
+	private final static int SERVER_PORT = 8100;
 	private Socket client = null;
 	private ObjectOutputStream oos;
 	private ObjectInputStream ois;
 	private String id;
-	private ReceiveThread rt;
+	private ReceiveDataThread rt;
 	private JButton btnConnect, send, exit;
 	private JTextField txtIP, txtName, txtInput;
-	private JTextArea txtList;
+	public static JTextArea txtList;
 	private CardLayout cl;
 	private JPanel p2;
 	private GameFrame gameFrame;
 	private static int line = 0;
 
 	public ChatFrame(GameFrame gameFrame) {
-		this.setAlwaysOnTop(true); // 항상 다른 JFrame보다 위에 있도록 설정
+		this.setAlwaysOnTop(true); //항상 다른 JFrame보다 위에 있도록 설정
 		this.gameFrame = gameFrame;
 		this.setLocationRelativeTo(this.gameFrame.getItemPanel());
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
-		this.setBounds((dim.width / 2) + (gameFrame.getWidth() / 5), (dim.height / 2) - (gameFrame.getHeight() / 30),
-				250, 400);
+		this.setBounds((dim.width/2)+(gameFrame.getWidth()/5), (dim.height/2)-(gameFrame.getHeight()/30), 250, 400);
 		cl = new CardLayout();
 
 		JPanel connect = new JPanel();
@@ -54,8 +55,12 @@ public class ChatFrame extends JFrame implements ActionListener {
 
 		JPanel connectSub = new JPanel();
 
-		connectSub.add(new JLabel("학번 : "));
-		txtName = new JTextField("", 17);
+		connectSub.add(new JLabel("Server IP: "));
+		txtIP = new JTextField("edu.gamgineer.online", 20);
+		connectSub.add(txtIP);
+
+		connectSub.add(new JLabel("이름: "));
+		txtName = new JTextField("korean", 17);
 		connectSub.add(txtName);
 
 		connect.add("Center", connectSub);
@@ -97,7 +102,7 @@ public class ChatFrame extends JFrame implements ActionListener {
 
 		cl.show(p2, "접속창");
 		setVisible(true);
-
+		
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) {
 				dispose();
@@ -107,7 +112,8 @@ public class ChatFrame extends JFrame implements ActionListener {
 	}
 
 	public void init() throws IOException {
-		client = new Socket(SERVER_IP, SERVER_PORT);
+		ip = txtIP.getText();
+		client = new Socket(ip, SERVER_PORT);
 		oos = new ObjectOutputStream(client.getOutputStream());
 		ois = new ObjectInputStream(client.getInputStream());
 
@@ -115,7 +121,7 @@ public class ChatFrame extends JFrame implements ActionListener {
 		oos.writeObject(id);
 		oos.flush();
 
-		rt = new ReceiveThread();
+		rt = new ReceiveDataThread(client, ois);
 		Thread t = new Thread(rt);
 		t.start();
 
@@ -133,9 +139,9 @@ public class ChatFrame extends JFrame implements ActionListener {
 				oos.flush();
 				txtInput.setText("");
 				this.line++;
-				if (line >= 20) {
+				if(line>=20) {
 					txtList.setText("");
-					line = 0;
+					line=0;
 				}
 			} else if (e.getSource() == exit)
 				System.exit(0);
@@ -144,19 +150,19 @@ public class ChatFrame extends JFrame implements ActionListener {
 		}
 	}
 
-	class ReceiveThread implements Runnable {
-		String receiveData;
-
-		public void run() {
-			while (true) {
-				try {
-					receiveData = (String) ois.readObject();
-					txtList.append(receiveData + "\n");
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		}
-	}
+//	class ReceiveThread implements Runnable {
+//		String receiveData;
+//
+//		public void run() {
+//			while (true) {
+//				try {
+//					receiveData = (String) ois.readObject();
+//					txtList.append(receiveData + "\n");
+//				} catch (Exception e) {
+//					e.printStackTrace();
+//				}
+//			}
+//		}
+//	}
 
 }
